d7d4128cbabe1549598cfdeebd4139ab83733587
Nygaaard <nygardandreas@hotmail.com>
2025-03-02 16:33:03 +0100

    Created install file

---
+++ install.js
@@ -0,0 +1,32 @@
+const sqlite = require("sqlite3").verbose();
+const db = new sqlite.Database("./blog.db");
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    `);
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            content TEXT NOT NULL,
+            userId INTEGER NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
+        )
+        `);
+    
+    console.log("Tabeller skapade...");
+})
+
+module.exports = db;
+
